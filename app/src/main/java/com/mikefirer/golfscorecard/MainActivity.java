e2201625705ea8193fc59992cee2c22a0793b9d6
Mike Firer <firerml@gmail.com>
2016-08-04 16:08:53 -0400

    Menu with one option

--- app/src/main/java/com/mikefirer/golfscorecard/MainActivity.java
+++ app/src/main/java/com/mikefirer/golfscorecard/MainActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     public static final String PREFS_FILE = "com.mikefirer.golfscorecard.preferences";
@@ -16,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private SharedPreferences mSharedPrefs;
     private SharedPreferences.Editor mEditor;
 
+    private RecyclerView mScoreRecyclerView;
+    private ScoreAdapter mScoreAdapter;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -23,15 +28,15 @@ public class MainActivity extends AppCompatActivity {
         mSharedPrefs = getSharedPreferences(PREFS_FILE, Context.MODE_PRIVATE);
         mEditor = mSharedPrefs.edit();
 
-        RecyclerView scoreRecyclerView = (RecyclerView) findViewById(R.id.scoreRecyclerView);
+        mScoreRecyclerView = (RecyclerView) findViewById(R.id.scoreRecyclerView);
 
-        ScoreAdapter adapter = new ScoreAdapter(mScores);
-        scoreRecyclerView.setAdapter(adapter);
-        scoreRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mScoreAdapter = new ScoreAdapter(mScores);
+        mScoreRecyclerView.setAdapter(mScoreAdapter);
+        mScoreRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
-        scoreRecyclerView.setHasFixedSize(true);
+        mScoreRecyclerView.setHasFixedSize(true);
 
-        for (int i = 0; i < 18; i++) {
+        for (int i = 0; i < mScores.length; i++) {
             String holeNumberKey = KEY_HOLE_NUMBER + (i + 1);
             mScores[i] = mSharedPrefs.getInt(holeNumberKey, 0);
         }
@@ -40,10 +45,39 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        for (int i = 0; i < 18; i++) {
+        for (int i = 0; i < mScores.length; i++) {
             String holeNumberKey = KEY_HOLE_NUMBER + (i + 1);
             mEditor.putInt(holeNumberKey, mScores[i]);
         }
         mEditor.apply();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu, adding items to the action bar if present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will automatically
+        // handle clicks on the Home/Up button, as long as you specify a parent
+        // activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        if (id == R.id.clear_strokes) {
+            mEditor.clear();
+            mEditor.apply();
+
+            for (int i = 0; i < mScores.length; i++) {
+                mScores[i] = 0;
+            }
+            mScoreAdapter.notifyDataSetChanged();
+
+            return true;
+        };
+
+        return super.onOptionsItemSelected(item);
     }
 }
